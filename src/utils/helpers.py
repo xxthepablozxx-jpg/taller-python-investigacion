8559ebae706bb5f9023dc37aff1f3453de3ca9fc
xxthepablozxx-jpg <xxthepablozxx@gmail.com>
2026-05-17 21:41:54 -0500

    feat: agregar módulo de utilidades

---
+++ src/utils/helpers.py
@@ -0,0 +1,9 @@
+"""
+Módulo de utilidades del proyecto
+"""
+
+def saludar(nombre):
+    return f"¡Hola, {nombre}!"
+
+def calcular_anio_nacimiento(edad, anio_actual=2026):
+    return anio_actual - edad
